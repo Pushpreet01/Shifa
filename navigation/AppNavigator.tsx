f51dae8397f328c77e2adb7868aa0109ccf56756
Dhruv Pabbi <161884115+dhruvxr@users.noreply.github.com>
2025-04-14 10:42:01 -0600

    Update AppNavigator.tsx

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,15 +1,16 @@
+// navigation/AppNavigator.tsx
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../app/LoginScreen';
 import EventsScreen from '../app/EventsScreen';
 import EventsFormScreen from '../app/EventsFormScreen';
-import RegisterEventScreen from '../app/RegisterEventScreen'; //  NEW IMPORT
+import RegisterEventScreen from '../app/RegisterEventScreen';
 
 export type RootStackParamList = {
   Login: undefined;
   Events: undefined;
   EventsForm: undefined;
-  RegisterEvent: { eventId: string }; //  NEW ROUTE
+  RegisterEvent: { eventId: string };
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
@@ -18,26 +19,10 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Events"
-          component={EventsScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="EventsForm"
-          component={EventsFormScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="RegisterEvent"
-          component={RegisterEventScreen}
-          options={{ headerShown: false }}
-        />
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Events" component={EventsScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="EventsForm" component={EventsFormScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="RegisterEvent" component={RegisterEventScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );

3bcea8cc7a4eac8d2eb592a9e031514c02d491ce
CraftyNuker <gallasicnoah@gmail.com>
2025-08-13 12:23:38 -0600

    Create MainActivity.kt

---
+++ android/app/src/main/java/com/chroniccoffee/shifa/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yourapp
+
+import com.facebook.react.ReactActivity
+
+class MainActivity: ReactActivity() {
+  /**
+   * Returns the name of the main component registered from JavaScript.
+   * This is used to schedule rendering of the component.
+   */
+  override fun getMainComponentName(): String = "main"
+}
